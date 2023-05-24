1662a674af73588b0e882fa5944f36dc551b91ac
Ivo Serra <ivoozmarserra@gmail.com>
2023-05-24 09:14:00 -0300

    server casi listo

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,64 +1,69 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import Cards from './components/Cards/Cards';
-import About from "./components/About.jsx"
-import Detail from "./components/Detail.jsx"
-import Nav from './components/Nav.jsx';
-import axios from 'axios';
-import { Route, Routes, useLocation, useNavigate} from 'react-router-dom';
-import Form from './components/Form';
-import Favorites from './components/Favorites';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Cards from "./components/Cards/Cards";
+import About from "./components/About.jsx";
+import Detail from "./components/Detail.jsx";
+import Nav from "./components/Nav.jsx";
+import axios from "axios";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import Form from "./components/Form";
+import Favorites from "./components/Favorites";
 
-const EMAIL = 'ivoozmarserra@gmail.com';
-const PASSWORD = 'asd123';
+
 
 function App() {
-   const [characters, setCharacters] = useState([]);
-   const {pathname} = useLocation();
-   const [access, setAccess] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const { pathname } = useLocation();
+  const [access, setAccess] = useState(false);
 
-   const navigate = useNavigate()
-   function onSearch(id) {
-      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(
-        ({ data }) => {
-          if (data.name) {
-            setCharacters((oldChars) => [...oldChars, data]);
-          } else {
-            window.alert("¡No hay personajes con este ID!");
-          }
+  const navigate = useNavigate();
+  function onSearch(id) {
+    axios.get(`http://localhost:3001/rickandmorty/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID!");
         }
-      );
-    }
-function login(userData) {
-   if (userData.password === PASSWORD && userData.email === EMAIL) {
-      setAccess(true);
-      navigate('/home');
-   }
-};
+      }
+    );
+  }
+  function login(userData) {
+    const { email, password } = userData;
+    const URL = "http://localhost:3001/rickandmorty/login/";
+    axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+      const { access } = data;
+      setAccess(access);
+      access && navigate("/home");
+    });
+  }
 
-useEffect(() => {
-   !access && navigate('/');
- }, [access, navigate]);
+  useEffect(() => {
+    !access && navigate("/");
+  }, [access, navigate]);
 
- const onClose = (id) => {
-   setCharacters(
-     characters.filter((char) => {
-       return char.id !== Number(id);
-     })
-   );
- };
-   return (
-      <div className='App'>
-      {pathname !== "/" && <Nav onSearch={onSearch}/>}
+  const onClose = (id) => {
+    setCharacters(
+      characters.filter((char) => {
+        return char.id !== Number(id);
+      })
+    );
+  };
+  return (
+    <div className="App">
+      {pathname !== "/" && <Nav onSearch={onSearch} />}
       <Routes>
-         <Route path='/' element= {<Form login={login}/>}/>
-         <Route path='/home' element={<Cards onClose={onClose} characters= {characters}/>} />   
-         <Route path='/about' element={<About/>}/>
-         <Route path='/detail/:id' element={<Detail/>}/>
-         <Route path='/favorites' element={<Favorites/>}/>
+        <Route path="/" element={<Form login={login} />} />
+        <Route
+          path="/home"
+          element={<Cards onClose={onClose} characters={characters} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
-      </div>
-   );
+    </div>
+  );
 }
 
 export default App;
